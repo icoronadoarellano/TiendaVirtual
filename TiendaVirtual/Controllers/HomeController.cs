8547ac782ad15623791435c425f08701e3d3a073
icoronadoarellano <icoronadoarellano@gmail.com>
2016-05-07 23:50:42 -0500

    Inicio de proyecto

---
+++ TiendaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaVirtual.Controllers
+{
+    public class HomeController : BaseController
+    {
+
+        public ActionResult CambiarCultura(string culturaSeleccionada)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(culturaSeleccionada);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(culturaSeleccionada);
+            Session["ActualCultura"] = culturaSeleccionada;
+            return View("Index");
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
